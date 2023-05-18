8ce93e96d52e348cedc92ce6a68389edbc20f52b
thanh289211 <122135348+thanh289211@users.noreply.github.com>
2023-05-18 08:19:25 +0700

    Update

--- MkbossManage/MkbossManage/src/main/java/com/mkboss/MkbossManage/Service/ProductService.java
+++ MkbossManage/MkbossManage/src/main/java/com/mkboss/MkbossManage/Service/ProductService.java
@@ -3,8 +3,8 @@ package com.mkboss.MkbossManage.Service;
 import com.mkboss.MkbossManage.Model.ProductModel;
 
 public interface ProductService {
-    public void addProduct(ProductModel productModel);
-    public void updateProduct(ProductModel productModel);
-    public void deleteProduct(ProductModel productModel);
-    public void getProducts();
+    void addProduct(ProductModel productModel);
+    void updateProduct(ProductModel productModel);
+    void deleteProduct(ProductModel productModel);
+    void getProducts();
 }
